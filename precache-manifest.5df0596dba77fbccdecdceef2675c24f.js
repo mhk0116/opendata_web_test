ab4107fef42d9a1bcac4a9d4710dcb55534c069b
Minhwan Kim <mhk0116@gmail.com>
2020-04-20 06:45:46 +0900

    Updates

---
+++ precache-manifest.5df0596dba77fbccdecdceef2675c24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e03d9c3313dae886232657c0d2f526",
+    "url": "/opendata_web_test/index.html"
+  },
+  {
+    "revision": "09b2b2309952e5323ceb",
+    "url": "/opendata_web_test/static/css/main.1bfcc921.chunk.css"
+  },
+  {
+    "revision": "1a0089a4bf227418a40d",
+    "url": "/opendata_web_test/static/js/2.cc525c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/opendata_web_test/static/js/2.cc525c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b2b2309952e5323ceb",
+    "url": "/opendata_web_test/static/js/main.57fa2535.chunk.js"
+  },
+  {
+    "revision": "285c4c123de982589b69",
+    "url": "/opendata_web_test/static/js/runtime-main.f2d56a1d.js"
+  }
+]);
